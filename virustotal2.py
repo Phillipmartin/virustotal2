a68e9be205daeda41b09d41c88bc77f2d056ffbd
Ryan Sears <fitblip@gmail.com>
2014-05-20 00:16:31 -0700

    Use requests module's multipart file

--- virustotal2.py
+++ virustotal2.py
@@ -10,8 +10,7 @@ import time
 import urllib2
 import urllib
 import hashlib
-import mimetypes
-import httplib
+import requests
 
 
 class VirusTotal2(object):
@@ -63,14 +62,11 @@ class VirusTotal2(object):
 
         elif thing_type == "file" and rescan is False:
             endpoint = "https://www.virustotal.com/vtapi/v2/file/scan"
-            fh = open(thing, 'rb')
-            content = fh.read()
-            host = "www.virustotal.com"
-            fields = [("apikey", self.api_key)]
-            files = [("file", thing, content)]
+            with open(thing, 'rb') as f:
+                fileContents = f.read()
 
             self._limit_call_handler()
-            result = postfile.post_multipart(host, endpoint, fields, files)
+            result = requests.post(endpoint, data=data, files={"file": (os.path.basename(thing), fileContents)}).text
         elif thing_type == "file" and rescan is True:
             endpoint = "https://www.virustotal.com/vtapi/v2/file/rescan"
             fh = open(thing, 'rb')
@@ -415,59 +411,3 @@ class VirusTotal2Report(object):
         while self.response_code not in (1, 0):
             time.sleep(interval)
             self.update()
-
-
-
-# copied from https://www.virustotal.com/en/documentation/public-api/#scanning-files
-# noinspection PyClassHasNoInit
-class postfile:
-    @staticmethod
-    def post_multipart(host, selector, fields, files):
-        """
-        Post fields and files to an http host as multipart/form-data.
-        fields is a sequence of (name, value) elements for regular form fields.
-        files is a sequence of (name, filename, value) elements for data to be uploaded as files
-        Return the server's response page.
-        """
-        content_type, body = postfile.encode_multipart_formdata(fields, files)
-        h = httplib.HTTPS(host)
-        h.putrequest('POST', selector)
-        h.putheader('content-type', content_type)
-        h.putheader('content-length', str(len(body)))
-        h.endheaders()
-        h.send(body)
-        errcode, errmsg, headers = h.getreply()
-
-        return h.file.read()
-
-    @staticmethod
-    def encode_multipart_formdata(fields, files):
-        """
-        fields is a sequence of (name, value) elements for regular form fields.
-        files is a sequence of (name, filename, value) elements for data to be uploaded as files
-        Return (content_type, body) ready for httplib.HTTP instance
-        """
-        BOUNDARY = '----------ThIs_Is_tHe_bouNdaRY_$'
-        CRLF = '\r\n'
-        L = []
-        for (key, value) in fields:
-            L.append('--' + BOUNDARY)
-            L.append('Content-Disposition: form-data; name="%s"' % key)
-            L.append('')
-            L.append(value)
-        for (key, filename, value) in files:
-            L.append('--' + BOUNDARY)
-            L.append('Content-Disposition: form-data; name="%s"; filename="%s"' % (key, filename))
-            L.append('Content-Type: %s' % postfile.get_content_type(filename))
-            L.append('')
-            L.append(value)
-        L.append('--' + BOUNDARY + '--')
-        L.append('')
-        body = CRLF.join((bytes(i) for i in L))
-        content_type = 'multipart/form-data; boundary=%s' % BOUNDARY
-
-        return content_type, body
-
-    @staticmethod
-    def get_content_type(filename):
-        return mimetypes.guess_type(filename)[0] or 'application/octet-stream'
